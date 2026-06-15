98bd2e27c3d62c5638f5ac6e82ea03ff04f5793a
NiushaEbrahimi <n.ebrahiminoor@gmail.com>
2026-06-15 19:59:31 +0330

    feat: added the Add Converstion button and a new slice for it

--- frontend/src/features/chat/ConversionList.tsx
+++ frontend/src/features/chat/ConversionList.tsx
@@ -1,9 +1,11 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { Bookmark, PlusCircle, X, Search } from 'lucide-react';
+import { useQuery } from '@tanstack/react-query';
+
 import { setActiveRoom } from '../../store/slices/chatSlice';
+import { setNewConvo, offNewConvo } from '../../store/slices/newConvo';
 import type { RootState } from '../../store';
 import type { Room } from '../../types/chatTypes';
-import { Bookmark } from 'lucide-react';
-import { useQuery } from '@tanstack/react-query';
 import { fetchSavedMessage } from '../../api/chat';
 import RoomAvatar from './components/RoomAvatar';
 
@@ -13,6 +15,7 @@ const ConversationList = () => {
   const activeRoomId = useSelector((s: RootState) => s.chat.activeRoomId);
   const onlineUserIds = useSelector((s: RootState) => s.chat.onlineUserIds);
   const currentUserId = useSelector((s: RootState) => s.auth.user?.id);
+  const newConvo = useSelector((s: RootState) => s.newConvo.isNewConvo);
 
   const { data: savedRoom } = useQuery({
     queryKey: ['saved-messages-room'],
@@ -21,12 +24,14 @@ const ConversationList = () => {
 
   return (
     <div className='flex flex-col h-full'>
+      {/* TODO: this should go to menu */}
       <div className='flex justify-between items-center p-4 border-b border-gray-400 shadow'>
         <h1 className='text-2xl font-semibold'>Chats</h1>
         <span className='flex cursor-pointer' onClick={()=>savedRoom && dispatch(setActiveRoom(savedRoom.data.id))}>
           <Bookmark/> saved
         </span>
       </div>
+
       <div style={{ overflowY: 'auto', flex: 1 }}>
         {rooms.map((room: Room) => {
           const isActive = room.id === activeRoomId;
@@ -75,9 +80,40 @@ const ConversationList = () => {
             </div>
           );
         })}
+        {/* TODO: this should go to menu */}
+        <div 
+          onClick={()=>{dispatch(setNewConvo())}} 
+          className='w-full flex justify-center mt-5 gap-2 cursor-pointer'
+        >
+          <PlusCircle/> Add
+        </div>
       </div>
+      {newConvo && <AddNewConverstaion/>}
     </div>
   );
 };
+
+function AddNewConverstaion(){
+  const dispatch = useDispatch()
+  return(
+    <main 
+      className='w-screen h-screen z-1000 absolute top-0 left-0 flex justify-center items-center' 
+      style={{backgroundColor : "rgba(255,255,255,0.7)"}}
+    >
+      <section className='w-1/2 h-2/3 bg-white border rounded-2xl shadow-2xl p-8 relative flex justify-center'>
+        <div 
+          className='absolute top-5 right-5 cursor-pointer rounded-2xl p-1 hover:border '
+          onClick={()=>{dispatch(offNewConvo())}}
+        >
+          <X/>
+        </div>
+        <div className='relative w-1/2'>
+          <input type="text" className='w-full border rounded-2xl p-2' placeholder='@...'/>
+          <Search className='absolute top-2 right-2'/>
+        </div>
+      </section>
+    </main>
+  )
+}
 
 export default ConversationList;
